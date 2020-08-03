055e66bb47cb75488509baaac0f8c8e1a8f2b6e1
Stefan Scherer <scherer_stefan@icloud.com>
2020-08-03 20:09:47 +0200

    Download hd

--- src/YouTv/Recording.js
+++ src/YouTv/Recording.js
@@ -14,7 +14,7 @@ export default class Recording {
     return this.data.recorded;
   }
 
-  async download(quality = 'hq') {
+  async download(quality = 'hd') {
     const { file } = this.data.files.filter(i => i.quality === quality)[0];
     return fetch(file, { token: this.token }).then(res => res.body);
   }
